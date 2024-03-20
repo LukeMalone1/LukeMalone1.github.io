436706207206fec920b7ee8206ab265eeba7e0e4
LukeMalone1 <Lucas.Malone@colorado.edu>
2024-03-20 17:36:00 -0600

    wa10

---
+++ wa/wa10.js
@@ -0,0 +1,12 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+function getName(){
+    button.textContent = `Player 1: ${name}`;
+}
